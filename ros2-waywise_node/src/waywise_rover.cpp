713ae2a151f5fc8adf48626a75ff7d86a2246696
Marvin Damschen <marvin.damschen@ri.se>
2023-10-13 16:00:46 +0200

    ros2-waywise_node: towards publishing odom and tf messages

--- ros2-waywise_node/src/waywise_rover.cpp
+++ ros2-waywise_node/src/waywise_rover.cpp
@@ -5,6 +5,9 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
+#include "nav_msgs/msg/odometry.hpp"
+#include "geometry_msgs/msg/transform_stamped.hpp"
+#include "tf2_ros/transform_broadcaster.h"
 
 using namespace std::chrono_literals;
 
@@ -14,29 +17,36 @@ using namespace std::chrono_literals;
 class WayWiseRover : public rclcpp::Node
 {
 public:
-  WayWiseRover()
-  : Node("waywise_rover"), count_(0)
-  {
-    publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
-    timer_ = this->create_wall_timer(
-      500ms, std::bind(&WayWiseRover::timer_callback, this));
+  WayWiseRover() : Node("waywise_rover"), count_(0) {
+    string_pub_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
+    odom_pub_ = create_publisher<nav_msgs::msg::Odometry>("odom", 10);
+    tf_pub_.reset(new tf2_ros::TransformBroadcaster(this));
+
+    timer_ = this->create_wall_timer(500ms, std::bind(&WayWiseRover::timer_callback, this));
   }
 
 private:
-  void timer_callback()
-  {
+  void timer_callback() {
     auto message = std_msgs::msg::String();
     message.data = "Hello, WayWise! " + std::to_string(count_++);
     RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
-    publisher_->publish(message);
+
+    auto odom = nav_msgs::msg::Odometry();
+    auto tf = geometry_msgs::msg::TransformStamped();
+
+    string_pub_->publish(message);
+    odom_pub_->publish(odom);
+    tf_pub_->sendTransform(tf);
   }
+
   rclcpp::TimerBase::SharedPtr timer_;
-  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr string_pub_;
+  rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr odom_pub_;
+  std::shared_ptr<tf2_ros::TransformBroadcaster> tf_pub_;
   size_t count_;
 };
 
-int main(int argc, char * argv[])
-{
+int main(int argc, char * argv[]) {
   rclcpp::init(argc, argv);
   rclcpp::spin(std::make_shared<WayWiseRover>());
   rclcpp::shutdown();
