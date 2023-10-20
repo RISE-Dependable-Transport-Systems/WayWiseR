3581f1dc7a997b3e447bbcefd3ebb4cf0c4ad923
r2avula <r2avula@gmail.com>
2023-10-20 12:33:09 +0200

    replaced teleop_tools with teleop_twist_joy

--- ros2-waywise_teleop/launch/teleop.launch.py
+++ ros2-waywise_teleop/launch/teleop.launch.py
@@ -7,33 +7,41 @@ from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
-def generate_launch_description():     
-    teleop_dir = get_package_share_directory('ros2-waywise_teleop')
-    
+
+def generate_launch_description():
+    teleop_dir = get_package_share_directory("ros2-waywise_teleop")
+
+    # args that can be set from the command line or a default will be used
     joy_la = DeclareLaunchArgument(
-        'joy_config', default_value=os.path.join(teleop_dir, 'config/joy_teleop.yaml'),
-        description='Full path to params file')
-    
+        "joy_config",
+        default_value=os.path.join(teleop_dir, "config/joy_teleop.yaml"),
+        description="Full path to params file",
+    )
+
+    # start nodes and use args to set parameters
     joy_node = Node(
-        package='joy',
-        executable='joy_node',
-        name='joy',
-        parameters=[LaunchConfiguration('joy_config')]
-    )    
-    joy_teleop_node = Node(
-        package='joy_teleop',
-        executable='joy_teleop',
-        name='joy_teleop',
-        parameters=[LaunchConfiguration('joy_config')]
-    )   
-    
+        package="joy",
+        executable="joy_node",
+        name="joy",
+        parameters=[LaunchConfiguration("joy_config")],
+    )
+
+    teleop_twist_joy_node = Node(
+        package="teleop_twist_joy",
+        executable="teleop_node",
+        name="teleop_twist_joy_node",
+        parameters=[LaunchConfiguration("joy_config")],
+        remappings={("/cmd_vel", "/cmd_vel_out")},
+    )
+
     # create launch description
     ld = LaunchDescription()
-    
+
     # declare launch args
     ld.add_action(joy_la)
-        
-    # start nodes    
+
+    # start nodes
     ld.add_action(joy_node)
-    ld.add_action(joy_teleop_node)
+    ld.add_action(teleop_twist_joy_node)
+
     return ld
