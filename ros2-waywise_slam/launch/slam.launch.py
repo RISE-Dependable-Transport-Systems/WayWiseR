81a4e1d648465b6ea43062df00192abd11b84aa1
Ramana Avula <r2avula@gmail.com>
2023-10-25 14:56:38 +0200

    added launch file & params for slam_toolbox node

---
+++ ros2-waywise_slam/launch/slam.launch.py
@@ -0,0 +1,50 @@
+import os
+
+from ament_index_python import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    slam_dir = get_package_share_directory("ros2-waywise_slam")
+    slam_toolbox_dir = get_package_share_directory("slam_toolbox")
+
+    # args that can be set from the command line or a default will be used
+    slam_config_la = DeclareLaunchArgument(
+        "slam_config",
+        default_value=os.path.join(slam_dir, "config/slam.yaml"),
+        description="Full path to params file for slam toolbox",
+    )
+    use_sim_time_la = DeclareLaunchArgument(
+        "use_sim_time", default_value="True", description="Use simulation/Gazebo clock"
+    )
+
+    # start nodes and use args to set parameters
+    slam_toolbox_node = Node(
+        package="slam_toolbox",
+        executable="async_slam_toolbox_node",
+        name="slam_toolbox",
+        output="screen",
+        parameters=[
+            LaunchConfiguration("slam_config"),
+            {
+                "use_sim_time": LaunchConfiguration("use_sim_time"),
+            },
+        ],
+        remappings={("/pose", "/visual_pose")},
+    )
+
+    # create launch description
+    ld = LaunchDescription()
+
+    # declare launch args
+    ld.add_action(slam_config_la)
+    ld.add_action(use_sim_time_la)
+
+    # start nodes
+    ld.add_action(slam_toolbox_node)
+
+    return ld
