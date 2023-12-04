c994918aa2160f0132fdc159b35f580c9309c579
Ramana Avula <r2avula@gmail.com>
2023-12-04 14:47:30 +0100

    ekf launch file structure updated

--- ros2-waywise_localization/launch/ekf.launch.py
+++ ros2-waywise_localization/launch/ekf.launch.py
@@ -1,35 +1,49 @@
-# Copyright 2018 Open Source Robotics Foundation, Inc.
-# Copyright 2019 Samsung Research America
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+import os
+
+from ament_index_python import get_package_share_directory
 
 from launch import LaunchDescription
-from ament_index_python.packages import get_package_share_directory
-import launch_ros.actions
-import os
-import yaml
-from launch.substitutions import EnvironmentVariable
-import pathlib
-import launch.actions
 from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
 
 def generate_launch_description():
-    return LaunchDescription([
-        launch_ros.actions.Node(
-            package='robot_localization',
-            executable='ekf_node',
-            name='ekf_filter_node',
-            output='screen',
-            parameters=[os.path.join(get_package_share_directory("robot_localization"), 'params', 'ekf.yaml')],
-           ),
-])
+    localisation_dir = get_package_share_directory("ros2-waywise_localization")
+
+    # args that can be set from the command line or a default will be used
+    use_sim_time_la = DeclareLaunchArgument(
+        "use_sim_time", default_value="True", description="Use simulation/Gazebo clock"
+    )
+    ekf_config_la = DeclareLaunchArgument(
+        "ekf_config",
+        default_value=os.path.join(localisation_dir, "config/ekf.yaml"),
+        description="Full path to params file for ekf_node",
+    )
+
+    # start nodes and use args to set parameters
+    ekf_filter_node = Node(
+        package="robot_localization",
+        executable="ekf_node",
+        name="ekf_filter_node",
+        output="screen",
+        parameters=[
+            LaunchConfiguration("ekf_config"),
+            {
+                "use_sim_time": LaunchConfiguration("use_sim_time"),
+            },
+        ],
+        remappings={("/odometry/filtered", "/odom/filtered")},
+    )
+    
+    # create launch description
+    ld = LaunchDescription()
+
+    # declare launch args
+    ld.add_action(use_sim_time_la)
+    ld.add_action(ekf_config_la)
+
+    # start nodes
+    ld.add_action(ekf_filter_node)
+
+    return ld
