c0208a8e590af4ab71815117112142c85cc8c49c
Ramana Avula <r2avula@gmail.com>
2023-12-10 16:09:20 +0100

    enable_keyboard param moved to teleop.yaml

--- ros2-waywise_teleop/launch/teleop.launch.py
+++ ros2-waywise_teleop/launch/teleop.launch.py
@@ -3,10 +3,11 @@ import os
 from ament_index_python import get_package_share_directory
 
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
+from launch.actions import DeclareLaunchArgument, OpaqueFunction
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 from launch.conditions import IfCondition
+import yaml
 
 
 def generate_launch_description():
@@ -17,12 +18,6 @@ def generate_launch_description():
         "teleop_config",
         default_value=os.path.join(teleop_dir, "config/teleop.yaml"),
         description="Full path to params file",
-    )
-
-    enable_keyboard_la = DeclareLaunchArgument(
-        "enable_keyboard",
-        default_value="false",
-        description="Enable keyboard",
     )
 
     # start nodes and use args to set parameters
@@ -41,16 +36,6 @@ def generate_launch_description():
         remappings={("/cmd_vel", "/joy_vel")},
     )
 
-    teleop_twist_keyboard_node = Node(
-        package="teleop_twist_keyboard",
-        executable="teleop_twist_keyboard",
-        name="teleop_twist_keyboard",
-        output="screen",
-        prefix="xterm -e",
-        condition=IfCondition(LaunchConfiguration("enable_keyboard")),
-        remappings={("/cmd_vel", "/key_vel")},
-    )
-
     teleop_gateway_node = Node(
         package="ros2-waywise_teleop",
         executable="teleop_gateway",
@@ -65,19 +50,41 @@ def generate_launch_description():
         parameters=[LaunchConfiguration("teleop_config")],
         remappings={("/cmd_vel_out", "/cmd_vel_mux")},
     )
+    
+    twist_keyboard_conditional_launch_action = OpaqueFunction(function = twist_keyboard_conditional_launch)   
 
     # create launch description
     ld = LaunchDescription()
 
     # declare launch args
     ld.add_action(teleop_la)
-    ld.add_action(enable_keyboard_la)
 
     # start nodes
     ld.add_action(joy_node)
     ld.add_action(teleop_twist_joy_node)
     ld.add_action(teleop_gateway_node)
-    ld.add_action(teleop_twist_keyboard_node)
+    ld.add_action(twist_keyboard_conditional_launch_action)
     ld.add_action(twist_mux_node)
 
     return ld
+
+def twist_keyboard_conditional_launch(context):
+    enable_keyboard = False
+    with open(LaunchConfiguration("teleop_config").perform(context)) as f:
+        config_data = yaml.safe_load(f)      
+        teleop_gateway_params_dict = config_data["teleop_gateway"]["ros__parameters"]   
+        if 'enable_keyboard' in teleop_gateway_params_dict:
+            enable_keyboard = teleop_gateway_params_dict["enable_keyboard"]
+        
+    teleop_twist_keyboard_node = Node(
+        package="teleop_twist_keyboard",
+        executable="teleop_twist_keyboard",
+        name="teleop_twist_keyboard",
+        output="screen",
+        prefix="xterm -e",
+        condition=IfCondition(str(enable_keyboard)),
+        remappings={("/cmd_vel", "/key_vel")},
+    )
+
+    return [teleop_twist_keyboard_node]
+
