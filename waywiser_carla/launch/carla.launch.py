3d0bfcf657ad1a3a245adf360ef49d97ec0cd02a
Ramana Avula <r2avula@gmail.com>
2024-11-26 16:25:51 +0100

    create carla orchestrator node

--- waywiser_carla/launch/carla.launch.py
+++ waywiser_carla/launch/carla.launch.py
@@ -23,50 +23,8 @@ def generate_launch_description():
         description='Full path to params file for carla',
     )
 
-    carla_spawn_objects_file_la = DeclareLaunchArgument(
-        'carla_spawn_objects_file',
-        default_value=os.path.join(waywiser_carla_dir, 'config/dts_truck_semitrailer.json'),
-        description='Full path to carla spawn objects definition file',
-    )
-
-    # start nodes and use args to set parameters
-    carla_ros_bridge = Node(
-        package='carla_ros_bridge',
-        executable='bridge',
-        name='carla_ros_bridge',
-        output='screen',
-        emulate_tty=True,
-        parameters=[
-            {'use_sim_time': LaunchConfiguration('use_sim_time')},
-            LaunchConfiguration('carla_config'),
-        ],
-    )
-
-    carla_spawn_objects = Node(
-        package='carla_spawn_objects',
-        executable='carla_spawn_objects',
-        name='carla_spawn_objects',
-        output='screen',
-        emulate_tty=True,
-        parameters=[
-            {'use_sim_time': LaunchConfiguration('use_sim_time')},
-            {'objects_definition_file': LaunchConfiguration('carla_spawn_objects_file')},
-            LaunchConfiguration('carla_config'),
-        ],
-    )
-
-    carla_initial_pose = Node(
-        package='carla_spawn_objects',
-        executable='set_initial_pose',
-        name='carla_initial_pose',
-        output='screen',
-        emulate_tty=True,
-        parameters=[
-            {'use_sim_time': LaunchConfiguration('use_sim_time')},
-            LaunchConfiguration('carla_config'),
-        ],
-    )
-
+    # use context to start nodes
+    carla_orchestrator_launch_action = OpaqueFunction(function=carla_orchestrator_launch)
     emulated_angle_sensor_conditional_launch_action = OpaqueFunction(
         function=emulated_angle_sensor_conditional_launch
     )
@@ -77,15 +35,69 @@ def generate_launch_description():
     # declare launch args
     ld.add_action(use_sim_time_la)
     ld.add_action(carla_config_la)
-    ld.add_action(carla_spawn_objects_file_la)
 
     # start nodes
-    ld.add_action(carla_ros_bridge)
-    ld.add_action(carla_spawn_objects)
-    ld.add_action(carla_initial_pose)
+    ld.add_action(carla_orchestrator_launch_action)
     ld.add_action(emulated_angle_sensor_conditional_launch_action)
 
     return ld
+
+
+def carla_orchestrator_launch(context):
+    with open(LaunchConfiguration('carla_config').perform(context)) as f:
+        config_data = yaml.safe_load(f)
+        node_params = config_data['/**']['ros__parameters']
+        node_params.update(config_data['carla_orchestrator_node']['ros__parameters'])
+
+    if 'carla_script_path' in node_params:
+        carla_script_path = node_params['carla_script_path']
+        carla_script_path = os.path.expanduser(carla_script_path)
+        node_params['carla_script_path'] = carla_script_path
+
+    if 'objects_json_path' in node_params:
+        objects_json_path = node_params['objects_json_path']
+        objects_json_path = os.path.expanduser(objects_json_path)
+
+        # If the path is relative, prepend the package's config directory
+        if objects_json_path and not objects_json_path.startswith('/'):
+            objects_json_path = os.path.join(
+                get_package_share_directory('waywiser_carla'),
+                'config/',
+                objects_json_path,
+            )
+        node_params['objects_json_path'] = objects_json_path
+
+    if 'sim_configurations_json_path' in node_params:
+        sim_configurations_json_path = node_params['sim_configurations_json_path']
+        sim_configurations_json_path = os.path.expanduser(sim_configurations_json_path)
+
+        # If the path is relative, prepend the package's config directory
+        if sim_configurations_json_path and not sim_configurations_json_path.startswith('/'):
+            sim_configurations_json_path = os.path.join(
+                get_package_share_directory('waywiser_carla'),
+                'config/',
+                sim_configurations_json_path,
+            )
+        node_params['sim_configurations_json_path'] = sim_configurations_json_path
+
+    if 'rosbag_output_dir' in node_params:
+        rosbag_output_dir = node_params['rosbag_output_dir']
+        rosbag_output_dir = os.path.expanduser(rosbag_output_dir)
+        node_params['rosbag_output_dir'] = rosbag_output_dir
+
+    carla_orchestrator = Node(
+        package='waywiser_carla',
+        executable='carla_orchestrator.py',
+        name='carla_orchestrator_node',
+        output='screen',
+        parameters=[
+            {'use_sim_time': LaunchConfiguration('use_sim_time')},
+            node_params,
+        ],
+        sigterm_timeout=['30'],
+    )
+
+    return [carla_orchestrator]
 
 
 def emulated_angle_sensor_conditional_launch(context):
