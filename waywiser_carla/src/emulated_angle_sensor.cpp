9d0adbaa9aa8e27d9e1995e31595bc8f82b7e2ab
Ramana Avula <r2avula@gmail.com>
2024-11-26 16:27:31 +0100

    waywiser_carla: update truck emulated angle sensor to use sim ready topic

--- waywiser_carla/src/emulated_angle_sensor.cpp
+++ waywiser_carla/src/emulated_angle_sensor.cpp
@@ -4,51 +4,63 @@
 #include "geometry_msgs/msg/transform_stamped.hpp"
 #include "std_msgs/msg/float32.hpp"
 #include <cmath>
+#include "std_msgs/msg/bool.hpp"
+
+using namespace std::placeholders;
 
 class EmulatedAngleSensor : public rclcpp::Node
 {
 public:
   EmulatedAngleSensor()
-  : Node("emulated_angle_sensor"), warmup_elapsed_(false)
+  : Node("emulated_angle_sensor")
   {
     primary_frame_ = this->declare_parameter<std::string>("primary_frame", "truck");
     secondary_frame_ = this->declare_parameter<std::string>("secondary_frame", "semitrailer");
     topic_ = this->declare_parameter<std::string>("topic", "sensors/angle");
     publish_rate_ = this->declare_parameter<int>("publish_rate", 10);
     invert_angle_ = this->declare_parameter<bool>("invert_angle", false);
-    warmup_time_ = this->declare_parameter<double>("warmup_time", 10.0);
 
     tf_buffer_ = std::make_shared<tf2_ros::Buffer>(this->get_clock());
     tf_listener_ = std::make_shared<tf2_ros::TransformListener>(*tf_buffer_);
 
     angle_pub_ = this->create_publisher<std_msgs::msg::Float32>(topic_, 10);
+    sim_ready_sub_ = this->create_subscription<std_msgs::msg::Bool>(
+      "/carla/simulation_ready", 10, std::bind(
+        &EmulatedAngleSensor::simReadyCallback, this, _1));
+
 
     // Initialize the timer for the publishing rate
     timer_ = this->create_wall_timer(
       std::chrono::milliseconds(1000 / publish_rate_),
       std::bind(&EmulatedAngleSensor::calculateAngle, this));
 
-    // Initialize the start time
-    start_time_ = this->get_clock()->now();
+
+    last_sim_time_ = this->get_clock()->now();
   }
 
 private:
+  void simReadyCallback(std_msgs::msg::Bool::SharedPtr msg)
+  {
+    if (msg->data) {
+      is_sim_ready_ = true;
+    }
+  }
+
   void calculateAngle()
   {
-    auto current_time = this->get_clock()->now();
-    auto elapsed_time = (current_time - start_time_).seconds();
+    if (is_sim_ready_) {
+      auto now = this->get_clock()->now();
 
-    if (elapsed_time < warmup_time_) {
-      // During warm-up time, no data is published
-      return;
-    } else {
-      // Warm-up period has elapsed
-      if (!warmup_elapsed_) {
-        warmup_elapsed_ = true;
-        RCLCPP_INFO(
-          this->get_logger(),
-          "Angle sensor warm-up period elapsed. Publishing data now.");
+      // Detect clock reset
+      if (now < last_sim_time_) {
+        RCLCPP_WARN(this->get_logger(), "Clock reset detected! Resetting initial angle.");
+        initial_angle_set_ = false;
+        is_sim_ready_ = false;
+        last_sim_time_ = now;
+        return;
       }
+
+      last_sim_time_ = now;
 
       geometry_msgs::msg::TransformStamped transformStamped;
       try {
@@ -93,6 +105,7 @@ private:
 
   rclcpp::TimerBase::SharedPtr timer_;
   rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr angle_pub_;
+  rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr sim_ready_sub_;
 
   std::shared_ptr<tf2_ros::Buffer> tf_buffer_;
   std::shared_ptr<tf2_ros::TransformListener> tf_listener_;
@@ -102,12 +115,11 @@ private:
   std::string topic_;
   int publish_rate_;
   bool invert_angle_;
-  double warmup_time_;
 
   bool initial_angle_set_;
-  bool warmup_elapsed_;
+  bool is_sim_ready_ = false;
   double initial_angle_;
-  rclcpp::Time start_time_;
+  rclcpp::Time last_sim_time_;
 };
 
 int main(int argc, char * argv[])
