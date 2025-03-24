51ffe49b569826a4932fce93853d977f8716c0a2
Ramana Avula <r2avula@gmail.com>
2025-03-24 20:46:57 +0100

    merge waywise_truck hw and ap files to a single src file

--- waywiser_core/cpp_src/waywise_truck.cpp
+++ waywiser_core/cpp_src/waywise_truck.cpp
@@ -10,7 +10,10 @@ void WayWiseTruck::setup_parameters()
 
   // Additional parameters for truck
   hitch_frame_ = declare_parameter("hitch_frame", base_frame_);
+  rear_axle_frame_to_hitch_frame_offset_ = declare_parameter(
+    "rear_axle_frame_to_hitch_frame_offset", std::vector<double>({0.0}));
 
+  #ifdef WAYWISE_HW_INTERFACE_
   // ToF Sensors
   tof_sensor_names = this->declare_parameter<std::vector<std::string>>(
     "tof_sensors", {}, rcl_interfaces::msg::ParameterDescriptor{});
@@ -35,6 +38,7 @@ void WayWiseTruck::setup_parameters()
     tof_sensor_info.topic_name = topic_name;
     tof_sensors_[tof_sensor_name] = tof_sensor_info;
   }
+  #endif
 
 
   has_trailer_ = this->declare_parameter("has_trailer", false);
@@ -45,7 +49,8 @@ void WayWiseTruck::setup_parameters()
     trailer_width_ = this->declare_parameter("trailer_width", 6.0);
     trailer_wheelbase_ = this->declare_parameter("trailer_wheelbase", 8.0);
 
-    angle_sensor_offset_ = this->declare_parameter("angle_sensor_offset", 0.0);
+    purepursuit_forward_gain_ = this->declare_parameter("purepursuit_forward_gain", 1.0);
+    purepursuit_reverse_gain_ = this->declare_parameter("purepursuit_reverse_gain", -1.0);
     angle_sensor_topic_ = this->declare_parameter("angle_sensor_topic", "/sensors/angle");
 
     trailer_rear_axle_frame_ = this->declare_parameter(
@@ -55,6 +60,15 @@ void WayWiseTruck::setup_parameters()
     trailer_rear_end_frame_ = this->declare_parameter("trailer_rear_end_frame", "");
     trailer_hitch_frame_ = this->declare_parameter("trailer_hitch_frame", "");
 
+    trailer_rear_axle_frame_to_trailer_base_frame_offset_ = declare_parameter(
+      "trailer_rear_axle_frame_to_trailer_base_frame_offset", std::vector<double>({0.0}));
+    trailer_rear_axle_frame_to_trailer_center_frame_offset_ = declare_parameter(
+      "trailer_rear_axle_frame_to_trailer_center_frame_offset", std::vector<double>({0.0}));
+    trailer_rear_axle_frame_to_trailer_rear_end_frame_offset_ = declare_parameter(
+      "trailer_rear_axle_frame_to_trailer_rear_end_frame_offset", std::vector<double>({0.0}));
+    trailer_rear_axle_frame_to_trailer_hitch_frame_offset_ = declare_parameter(
+      "trailer_rear_axle_frame_to_trailer_hitch_frame_offset", std::vector<double>({0.0}));
+
     trailer_wheel_joint_names_ = declare_parameter<std::vector<std::string>>(
       "trailer_wheel_joint_names",
       std::vector<std::string>{"semitrailer_rlw_link_joint", "semitrailer_rrw_link_joint"}
@@ -63,6 +77,11 @@ void WayWiseTruck::setup_parameters()
       "truck_trailer_link_joint_name", "truck_trailer_link_joint"
     );
     invert_trailer_joint_state_ = this->declare_parameter("invert_trailer_joint_state", false);
+    trailer_pose_topic_ = declare_parameter("trailer_pose_topic", "/trailer_pose");
+
+    #ifdef WAYWISE_HW_INTERFACE_
+    angle_sensor_offset_ = this->declare_parameter("angle_sensor_offset", 0.0);
+    #endif
   }
 }
 
@@ -72,6 +91,12 @@ void WayWiseTruck::setup_publishers()
   WayWiseCar::setup_publishers();
 
   // Additional publishers for truck
+  if (has_trailer_) {
+    trailer_pose_pub_ =
+      create_publisher<geometry_msgs::msg::PoseStamped>(trailer_pose_topic_, 10);
+  }
+
+  #ifdef WAYWISE_HW_INTERFACE_
   for (const auto & tof_sensor_name : tof_sensor_names) {
     tof_sensors_[tof_sensor_name].publisher =
       this->create_publisher<std_msgs::msg::Float32>(tof_sensors_[tof_sensor_name].topic_name, 10);
@@ -80,12 +105,22 @@ void WayWiseTruck::setup_publishers()
   if (has_trailer_) {
     angle_pub_ = this->create_publisher<std_msgs::msg::Float32>(angle_sensor_topic_, 10);
   }
+  #endif
 }
 
 void WayWiseTruck::setup_subscribers()
 {
   // Call base class setup_subscribers
   WayWiseCar::setup_subscribers();
+
+  // Additional subscribers for truck
+  #ifndef WAYWISE_HW_INTERFACE_
+  if (has_trailer_) {
+    angle_sub_ = this->create_subscription<std_msgs::msg::Float32>(
+      angle_sensor_topic_, 10,
+      std::bind(&WayWiseTruck::angle_sensor_callback, this, _1));
+  }
+  #endif
 }
 
 void WayWiseTruck::setup_timers()
@@ -94,33 +129,138 @@ void WayWiseTruck::setup_timers()
   WayWiseCar::setup_timers();
 }
 
-void WayWiseTruck::setup_hardware()
+void WayWiseTruck::setup_autopilot()
 {
   mTruckState.reset(new TruckState);
 
   // Additional setup for truck
+  mTruckState->setPurePursuitForwardGain(purepursuit_forward_gain_);
+  mTruckState->setPurePursuitReverseGain(purepursuit_reverse_gain_);
   if (has_trailer_) {
-    mTrailerState.reset(new TrailerState((int) MAV_COMP_ID_USER1, Qt::white));
+    mTrailerMavlinkComponentID = (int) MAV_COMP_ID_USER1;
+
+    mTrailerState.reset(new TrailerState(mTrailerMavlinkComponentID, Qt::white));
     mTrailerState->setLength(trailer_length_);
     mTrailerState->setWidth(trailer_width_);
     mTrailerState->setWheelBase(trailer_wheelbase_);
 
-    if (loadURDFFile()) {
-      Eigen::Vector3d offset = getLinkPosition(urdfModel, trailer_center_frame_) -
-        getLinkPosition(urdfModel, trailer_rear_axle_frame_);
-      mTrailerState->setRearAxleToCenterOffset(xyz_t{offset.x(), offset.y(), offset.z()});
+    // --- Set trailer rear axle offsets ---
+    bool offset_params_initiazed = true;
+    if (trailer_rear_axle_frame_to_trailer_base_frame_offset_.size() != 3 ||
+      trailer_rear_axle_frame_to_trailer_center_frame_offset_.size() != 3 ||
+      trailer_rear_axle_frame_to_trailer_rear_end_frame_offset_.size() != 3 ||
+      trailer_rear_axle_frame_to_trailer_hitch_frame_offset_.size() != 3)
+    {
+      offset_params_initiazed = false;
+    }
 
-      offset = getLinkPosition(urdfModel, trailer_rear_end_frame_) -
-        getLinkPosition(urdfModel, trailer_rear_axle_frame_);
-      mTrailerState->setRearAxleToRearEndOffset(xyz_t{offset.x(), offset.y(), offset.z()});
+    if (trailer_rear_axle_frame_to_trailer_center_frame_offset_.size() == 3) {
+      mTrailerState->setRearAxleToCenterOffset(
+        xyz_t{
+        trailer_rear_axle_frame_to_trailer_center_frame_offset_[0],
+        trailer_rear_axle_frame_to_trailer_center_frame_offset_[1],
+        trailer_rear_axle_frame_to_trailer_center_frame_offset_[2]
+      });
+    }
 
-      offset = getLinkPosition(urdfModel, trailer_hitch_frame_) -
-        getLinkPosition(urdfModel, trailer_rear_axle_frame_);
-      mTrailerState->setRearAxleToHitchOffset(xyz_t{offset.x(), offset.y(), offset.z()});
+    if (trailer_rear_axle_frame_to_trailer_rear_end_frame_offset_.size() == 3) {
+      mTrailerState->setRearAxleToRearEndOffset(
+        xyz_t{
+        trailer_rear_axle_frame_to_trailer_rear_end_frame_offset_[0],
+        trailer_rear_axle_frame_to_trailer_rear_end_frame_offset_[1],
+        trailer_rear_axle_frame_to_trailer_rear_end_frame_offset_[2]
+      });
+    }
+
+    if (trailer_rear_axle_frame_to_trailer_hitch_frame_offset_.size() == 3) {
+      mTrailerState->setRearAxleToHitchOffset(
+        xyz_t{
+        trailer_rear_axle_frame_to_trailer_hitch_frame_offset_[0],
+        trailer_rear_axle_frame_to_trailer_hitch_frame_offset_[1],
+        trailer_rear_axle_frame_to_trailer_hitch_frame_offset_[2]
+      });
+    }
+
+    if (!offset_params_initiazed && loadURDFFile()) {
+      Eigen::Vector3d offset;
+
+      if (trailer_rear_axle_frame_to_trailer_base_frame_offset_.size() != 3) {
+        offset = getLinkPosition(urdfModel, trailer_base_frame_) -
+          getLinkPosition(urdfModel, trailer_rear_axle_frame_);
+        trailer_rear_axle_frame_to_trailer_base_frame_offset_ = {offset.x(), offset.y(),
+          offset.z()};
+      }
+
+      if (trailer_rear_axle_frame_to_trailer_center_frame_offset_.size() != 3) {
+        offset = getLinkPosition(urdfModel, trailer_center_frame_) -
+          getLinkPosition(urdfModel, trailer_rear_axle_frame_);
+        trailer_rear_axle_frame_to_trailer_center_frame_offset_ = {offset.x(), offset.y(),
+          offset.z()};
+        mTrailerState->setRearAxleToCenterOffset(xyz_t{offset.x(), offset.y(), offset.z()});
+      }
+
+      if (trailer_rear_axle_frame_to_trailer_rear_end_frame_offset_.size() != 3) {
+        offset = getLinkPosition(urdfModel, trailer_rear_end_frame_) -
+          getLinkPosition(urdfModel, trailer_rear_axle_frame_);
+        trailer_rear_axle_frame_to_trailer_rear_end_frame_offset_ = {offset.x(), offset.y(),
+          offset.z()};
+        mTrailerState->setRearAxleToRearEndOffset(xyz_t{offset.x(), offset.y(), offset.z()});
+      }
+
+      if (trailer_rear_axle_frame_to_trailer_hitch_frame_offset_.size() != 3) {
+        offset = getLinkPosition(urdfModel, trailer_hitch_frame_) -
+          getLinkPosition(urdfModel, trailer_rear_axle_frame_);
+        trailer_rear_axle_frame_to_trailer_hitch_frame_offset_ = {offset.x(), offset.y(),
+          offset.z()};
+        mTrailerState->setRearAxleToHitchOffset(xyz_t{offset.x(), offset.y(), offset.z()});
+      }
+    } else {
+      if (trailer_rear_axle_frame_to_trailer_base_frame_offset_.size() != 3) {
+        trailer_rear_axle_frame_to_trailer_base_frame_offset_ = {0.0, 0.0, 0.0};
+      }
+
+      if (trailer_rear_axle_frame_to_trailer_center_frame_offset_.size() != 3) {
+        trailer_rear_axle_frame_to_trailer_center_frame_offset_ = {0.0, 0.0, 0.0};
+      }
+
+      if (trailer_rear_axle_frame_to_trailer_rear_end_frame_offset_.size() != 3) {
+        trailer_rear_axle_frame_to_trailer_rear_end_frame_offset_ = {0.0, 0.0, 0.0};
+      }
+
+      if (trailer_rear_axle_frame_to_trailer_hitch_frame_offset_.size() != 3) {
+        trailer_rear_axle_frame_to_trailer_hitch_frame_offset_ = {0.0, 0.0, 0.0};
+      }
     }
 
     mTruckState->setTrailingVehicle(mTrailerState);
+  }
 
+  // Call base class setup_hardware with mTruckState
+  WayWiseCar::setup_autopilot(mTruckState);
+
+  // Additional setup for truck
+  if (rear_axle_frame_to_hitch_frame_offset_.size() == 3) {
+    mTruckState->setRearAxleToHitchOffset(
+      xyz_t{
+      rear_axle_frame_to_hitch_frame_offset_[0],
+      rear_axle_frame_to_hitch_frame_offset_[1],
+      rear_axle_frame_to_hitch_frame_offset_[2]
+    });
+  } else if (loadURDFFile()) {
+    Eigen::Vector3d offset = getLinkPosition(urdfModel, hitch_frame_) -
+      getLinkPosition(urdfModel, rear_axle_frame_);
+    rear_axle_frame_to_hitch_frame_offset_ = {offset.x(), offset.y(), offset.z()};
+    mTruckState->setRearAxleToHitchOffset(xyz_t{offset.x(), offset.y(), offset.z()});
+  } else {
+    rear_axle_frame_to_hitch_frame_offset_ = {0.0, 0.0, 0.0};
+  }
+}
+
+#ifdef WAYWISE_HW_INTERFACE_
+void WayWiseTruck::setup_hardware()
+{
+  // Additional setup for truck
+  if (has_trailer_) {
     // ToF Sensors
     for (const auto & tof_sensor_name : tof_sensor_names) {
       ToFSensorInfo tof_sensor_info = tof_sensors_[tof_sensor_name];
@@ -135,59 +275,21 @@ void WayWiseTruck::setup_hardware()
 
     // Angle Sensor
     mAngleSensorUpdater.reset(new AS5600Updater(mTruckState, angle_sensor_offset_));
-    mTruckState->setSimulateTrailer(!mAngleSensorUpdater->isConnected());
+    if (!mAngleSensorUpdater->isConnected()) {
+      mTruckState->setSimulateTrailer(true);
+      RCLCPP_WARN(
+        this->get_logger(),
+        "Angle sensor not connected. Trailer angle will be simulated.");
+    }
   }
 
   // Call base class setup_hardware with mTruckState
-  WayWiseCar::setup_hardware(mTruckState);
-
-  // Additional setup for truck
-  if (loadURDFFile()) {
-    Eigen::Vector3d offset = getLinkPosition(urdfModel, hitch_frame_) -
-      getLinkPosition(urdfModel, rear_axle_frame_);
-    mTruckState->setRearAxleToHitchOffset(xyz_t{offset.x(), offset.y(), offset.z()});
-  }
+  WayWiseCar::setup_hardware();
 }
+#endif
 
-void WayWiseTruck::publish_odom_and_tfs(double timePassed_ms)
-{
-  WayWiseCar::publish_odom_and_tfs(timePassed_ms);
-  if (has_trailer_) {
-    publish_trailer_angle();
-
-    if (publish_odom_to_baselink_tf_) {
-      PosPoint odom_to_trailer_base_link_position = mTrailerState->posInVehicleFrameToPosPointENU(
-        mTrailerState->getRearAxleToCenterOffset(), PosType::odom);
-
-      double trailer_x = odom_to_trailer_base_link_position.getX();
-      double trailer_y = odom_to_trailer_base_link_position.getY();
-      double trailer_yaw_rad = -odom_to_trailer_base_link_position.getYaw() * M_PI / 180.0;
-
-      auto odom_to_trailer_msg_tf = geometry_msgs::msg::Transform();
-      odom_to_trailer_msg_tf.translation.x = trailer_x;
-      odom_to_trailer_msg_tf.translation.y = trailer_y;
-      odom_to_trailer_msg_tf.rotation.z = sin(trailer_yaw_rad / 2.0);
-      odom_to_trailer_msg_tf.rotation.w = cos(trailer_yaw_rad / 2.0);
-
-      auto odom_to_trailer_msg_tfs = geometry_msgs::msg::TransformStamped();
-      odom_to_trailer_msg_tfs.header.frame_id = odom_frame_;
-      odom_to_trailer_msg_tfs.child_frame_id = trailer_base_frame_;
-      odom_to_trailer_msg_tfs.header.stamp = now();
-      odom_to_trailer_msg_tfs.transform = odom_to_trailer_msg_tf;
-
-      // -- Publish Transform
-      tf_pub_->sendTransform(odom_to_trailer_msg_tfs);
-    }
-  }
-}
-
-void WayWiseTruck::publish_trailer_angle()
-{
-  std_msgs::msg::Float32 angle_msg;
-  angle_msg.data = mTruckState->getTrailerAngleDegrees();
-  angle_pub_->publish(angle_msg);
-}
-
+// ----------------- Callback methods -----------------
+#ifdef WAYWISE_HW_INTERFACE_
 void WayWiseTruck::updated_tof_distance_callback(
   const std::string & tof_sensor_name,
   double distance_m)
@@ -200,6 +302,33 @@ void WayWiseTruck::updated_tof_distance_callback(
   RCLCPP_INFO(
     this->get_logger(), "Published ToF distance %.2f from %s", distance_m,
     tof_sensor_name.c_str());
+}
+#else
+void WayWiseTruck::angle_sensor_callback(const std_msgs::msg::Float32::SharedPtr angle_msg)
+{
+  mTruckState->setTrailerAngle(angle_msg->data);
+}
+#endif
+
+// ----------------- Utility methods -----------------
+void WayWiseTruck::update_world_positon(geometry_msgs::msg::Pose world_pose)
+{
+  WayWiseCar::update_world_positon(world_pose);
+
+  if (has_trailer_) {
+    geometry_msgs::msg::PoseStamped world_pose_stamped;
+    world_pose_stamped.header.frame_id = world_frame_;
+    world_pose_stamped.header.stamp = this->get_clock()->now();
+
+    PosPoint currentTrailerPosition = mTrailerState->getPosition(PosType::fused);
+    world_pose_stamped.pose.position.x = currentTrailerPosition.getX();
+    world_pose_stamped.pose.position.y = currentTrailerPosition.getY();
+    world_pose_stamped.pose.position.z = currentTrailerPosition.getHeight();
+    tf2::Quaternion orientation;
+    orientation.setRPY(0.0, 0.0, currentTrailerPosition.getYaw() * M_PI / 180.0);
+    world_pose_stamped.pose.orientation = tf2::toMsg(orientation);
+    trailer_pose_pub_->publish(world_pose_stamped);
+  }
 }
 
 double WayWiseTruck::update_joint_states_msg(
@@ -218,14 +347,63 @@ double WayWiseTruck::update_joint_states_msg(
       joint_state_msg.velocity.push_back(wheel_rad_per_sec); // Wheel velocity in rad/s
       joint_state_msg.effort.push_back(0.0); // Effort (not used here)
     }
-  }
 
-  joint_state_msg.name.push_back(truck_trailer_link_joint_name_);
-  joint_state_msg.position.push_back(
-    invert_trailer_joint_state_ ? -mTruckState->getTrailerAngleRadians() : mTruckState->getTrailerAngleRadians()
-  );
-  joint_state_msg.velocity.push_back(0.0);   // Velocity in rad/s
-  joint_state_msg.effort.push_back(0.0);   // Effort (not used here)
+    joint_state_msg.name.push_back(truck_trailer_link_joint_name_);
+    joint_state_msg.position.push_back(
+      invert_trailer_joint_state_ ? -mTruckState->getTrailerAngleRadians() :
+      mTruckState->getTrailerAngleRadians()
+    );
+    joint_state_msg.velocity.push_back(0.0);   // Velocity in rad/s
+    joint_state_msg.effort.push_back(0.0);   // Effort (not used here)
+  }
 
   return wheel_position;
 }
+
+
+#ifdef WAYWISE_HW_INTERFACE_
+void WayWiseTruck::publish_odom_and_tfs()
+{
+  WayWiseCar::publish_odom_and_tfs();
+
+  if (has_trailer_) {
+    if (publish_odom_to_baselink_tf_) {
+      static xyz_t trailer_rear_axle_frame_to_trailer_base_frame_offset_xyz = {
+        trailer_rear_axle_frame_to_trailer_base_frame_offset_[0],
+        trailer_rear_axle_frame_to_trailer_base_frame_offset_[1],
+        trailer_rear_axle_frame_to_trailer_base_frame_offset_[2]
+      };
+      PosPoint odom_to_trailer_base_link_position = mTrailerState->posInVehicleFrameToPosPointENU(
+        trailer_rear_axle_frame_to_trailer_base_frame_offset_xyz, PosType::odom);
+
+      double trailer_x = odom_to_trailer_base_link_position.getX();
+      double trailer_y = odom_to_trailer_base_link_position.getY();
+      double trailer_yaw_rad = odom_to_trailer_base_link_position.getYaw() * M_PI / 180.0;
+
+      auto odom_to_trailer_msg_tf = geometry_msgs::msg::Transform();
+      odom_to_trailer_msg_tf.translation.x = trailer_x;
+      odom_to_trailer_msg_tf.translation.y = trailer_y;
+      odom_to_trailer_msg_tf.rotation.z = sin(trailer_yaw_rad / 2.0);
+      odom_to_trailer_msg_tf.rotation.w = cos(trailer_yaw_rad / 2.0);
+
+      auto odom_to_trailer_msg_tfs = geometry_msgs::msg::TransformStamped();
+      odom_to_trailer_msg_tfs.header.frame_id = odom_frame_;
+      odom_to_trailer_msg_tfs.child_frame_id = trailer_base_frame_;
+      odom_to_trailer_msg_tfs.header.stamp = now();
+      odom_to_trailer_msg_tfs.transform = odom_to_trailer_msg_tf;
+
+      // -- Publish Transform
+      tf_pub_->sendTransform(odom_to_trailer_msg_tfs);
+    }
+
+    publish_trailer_angle();
+  }
+}
+
+void WayWiseTruck::publish_trailer_angle()
+{
+  std_msgs::msg::Float32 angle_msg;
+  angle_msg.data = mTruckState->getTrailerAngleDegrees();
+  angle_pub_->publish(angle_msg);
+}
+#endif
--- waywiser_core/include/waywise_truck.hpp
+++ waywiser_core/include/waywise_truck.hpp
@@ -1,15 +1,16 @@
 #ifndef WAYWISE_TRUCK_HPP_
 #define WAYWISE_TRUCK_HPP_
 
-#include <map>
-
-#include "waywise_car.hpp"
-#include "WayWise/sensors/angle/as5600updater.h"
-#include "WayWise/sensors/tof/vl53l0xtofsensor.h"
 #include "WayWise/vehicles/truckstate.h"
 #include "WayWise/vehicles/trailerstate.h"
 
-#include "std_msgs/msg/float32.hpp"
+#ifdef WAYWISE_HW_INTERFACE_
+#include <map>
+#include "WayWise/sensors/angle/as5600updater.h"
+#include "WayWise/sensors/tof/vl53l0xtofsensor.h"
+#endif
+
+#include "waywise_car.hpp"
 
 class WayWiseTruck : public WayWiseCar
 {
@@ -23,33 +24,40 @@ public:
 
   virtual ~WayWiseTruck() = default;
 
-  // virtual void initialize_node() override;
-
 protected:
   virtual void setup_parameters() override;
   virtual void setup_publishers() override;
   virtual void setup_subscribers() override;
   virtual void setup_timers() override;
+  virtual void setup_autopilot() override;
+  #ifdef WAYWISE_HW_INTERFACE_
   virtual void setup_hardware() override;
+  #endif
 
   // Callback methods
-  virtual void publish_odom_and_tfs(double timePassed_ms) override;
-  void publish_trailer_angle();
+  #ifdef WAYWISE_HW_INTERFACE_
   void updated_tof_distance_callback(const std::string & tof_sensor_name, double distance_m);
+  #else
+  void angle_sensor_callback(const std_msgs::msg::Float32::SharedPtr angle_msg);
+  #endif
 
   // Utility methods
+  virtual void update_world_positon(geometry_msgs::msg::Pose world_pose) override;
   virtual double update_joint_states_msg(
     sensor_msgs::msg::JointState & joint_state_msg,
     double timePassed_ms) override;
+  #ifdef WAYWISE_HW_INTERFACE_
+  virtual void publish_odom_and_tfs() override;
+  void publish_trailer_angle();
+  #endif
 
   // ROS parameters
+  float purepursuit_forward_gain_, purepursuit_reverse_gain_;
+
   std::string hitch_frame_;
   bool has_trailer_;
   float trailer_length_, trailer_width_, trailer_wheelbase_;
-  float angle_sensor_offset_;
   std::string angle_sensor_topic_;
-
-  std::vector<std::string> tof_sensor_names;
 
   std::string trailer_base_frame_;
   std::string trailer_rear_axle_frame_;
@@ -57,22 +65,45 @@ protected:
   std::string trailer_rear_end_frame_;
   std::string trailer_hitch_frame_;
 
+  std::vector<double> rear_axle_frame_to_hitch_frame_offset_;
+  std::vector<double> trailer_rear_axle_frame_to_trailer_base_frame_offset_;
+  std::vector<double> trailer_rear_axle_frame_to_trailer_center_frame_offset_;
+  std::vector<double> trailer_rear_axle_frame_to_trailer_rear_end_frame_offset_;
+  std::vector<double> trailer_rear_axle_frame_to_trailer_hitch_frame_offset_;
+
   std::vector<std::string> trailer_wheel_joint_names_;
   std::string truck_trailer_link_joint_name_;
   bool invert_trailer_joint_state_;
 
+  std::string trailer_pose_topic_;
+
+  #ifdef WAYWISE_HW_INTERFACE_
+  float angle_sensor_offset_;
+  std::vector<std::string> tof_sensor_names;
+  #endif
+
   // Publishers
+  rclcpp::Publisher<geometry_msgs::msg::PoseStamped>::SharedPtr trailer_pose_pub_;
+  #ifdef WAYWISE_HW_INTERFACE_
   rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr angle_pub_;
+  #endif
 
   // Subscribers
+  #ifndef WAYWISE_HW_INTERFACE_
+  rclcpp::Subscription<std_msgs::msg::Float32>::SharedPtr angle_sub_;
+  #endif
 
   // WayWise components
   QSharedPointer<TruckState> mTruckState;
   QSharedPointer<TrailerState> mTrailerState;
+  #ifdef WAYWISE_HW_INTERFACE_
   QSharedPointer<AngleSensorUpdater> mAngleSensorUpdater;
   QSharedPointer<ToFSensor> mToFSensor;
+  #endif
 
   // Internal variables
+  int mTrailerMavlinkComponentID = -1;
+  #ifdef WAYWISE_HW_INTERFACE_
   struct ToFSensorInfo
   {
     int i2c_addr;
@@ -81,6 +112,7 @@ protected:
     rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr publisher;
   };
   std::map<std::string, ToFSensorInfo> tof_sensors_;
+  #endif
 
 // private:
 };
