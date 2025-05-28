60c4028467c6d46827ffe0710cc4e028f082e3c6
Ramana Avula <r2avula@gmail.com>
2025-05-28 09:17:45 +0200

    jazzy migration: fix cv_bridge header in waywiser_carla

--- waywiser_carla/cpp_src/emulated_range_sensor_array.cpp
+++ waywiser_carla/cpp_src/emulated_range_sensor_array.cpp
@@ -3,7 +3,7 @@
 #include "sensor_msgs/msg/range.hpp"
 #include "std_msgs/msg/bool.hpp"
 #include <opencv2/opencv.hpp>
-#include <cv_bridge/cv_bridge.h>
+#include <cv_bridge/cv_bridge.hpp>
 #include <map>
 
 using namespace std::placeholders;
