50530dff61d94fb76772241ffac4926e7bc1885f
Ramana Avula <r2avula@gmail.com>
2025-07-16 12:03:04 +0200

    move depthai node to hwbringup package

--- waywiser_hwbringup/launch/depthai.launch.py
+++ waywiser_hwbringup/launch/depthai.launch.py
@@ -8,18 +8,18 @@ from launch_ros.actions import Node
 
 
 def generate_launch_description():
-    waywiser_perception_dir = get_package_share_directory('waywiser_perception')
+    waywiser_hwbringup_dir = get_package_share_directory('waywiser_hwbringup')
 
     # args that can be set from the command line or a default will be used
     depthai_config_la = DeclareLaunchArgument(
         'depthai_config',
-        default_value=os.path.join(waywiser_perception_dir, 'config/depthai.yaml'),
+        default_value=os.path.join(waywiser_hwbringup_dir, 'config/depthai.yaml'),
         description='Full path to params file for DepthAI node.',
     )
 
     # start nodes and use args to set parameters
     depthai_node = Node(
-        package='waywiser_perception',
+        package='waywiser_hwbringup',
         executable='depthai_node.py',
         name='depthai_node',
         parameters=[
