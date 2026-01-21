8a7394dd0bba53ff89d0282ac8fc67cc949e9da8
Aria Mirzai <ariamirzai@outlook.com>
2026-01-21 16:56:54 +0100

    Formatting aligned with Developer Guide

--- waywiser_perception/launch/octomap.launch.py
+++ waywiser_perception/launch/octomap.launch.py
@@ -9,45 +9,45 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
     # Get package directory
-    waywiser_perception_dir = get_package_share_directory("waywiser_perception")
+    waywiser_perception_dir = get_package_share_directory('waywiser_perception')
 
     # Declare launch arguments
     octomap_config_la = DeclareLaunchArgument(
-        "octomap_config",
-        default_value=os.path.join(waywiser_perception_dir, "config/octomap.yaml"),
-        description="Full path to params file for OctoMap",
+        'octomap_config',
+        default_value=os.path.join(waywiser_perception_dir, 'config/octomap.yaml'),
+        description='Full path to params file for OctoMap',
     )
 
     use_sim_time_la = DeclareLaunchArgument(
-        "use_sim_time",
-        default_value="False",
-        description="Use simulation time/Gazebo clock",
+        'use_sim_time',
+        default_value='False',
+        description='Use simulation time/Gazebo clock',
     )
 
     # OctomapNode - creates octomaps from point clouds
     octomap_node = Node(
-        package="waywiser_perception",
-        executable="octomap_node",
-        name="octomap_node",
-        output="screen",
+        package='waywiser_perception',
+        executable='octomap_node',
+        name='octomap_node',
+        output='screen',
         parameters=[
-            LaunchConfiguration("octomap_config"),
+            LaunchConfiguration('octomap_config'),
             {
-                "use_sim_time": LaunchConfiguration("use_sim_time"),
+                'use_sim_time': LaunchConfiguration('use_sim_time'),
             },
         ],
     )
 
     # OctomapOcclusionsNode - detects occlusions and triggers emergency stops
     octomap_occlusions_node = Node(
-        package="waywiser_perception",
-        executable="octomap_occlusions_node",
-        name="octomap_occlusions_node",
-        output="screen",
+        package='waywiser_perception',
+        executable='octomap_occlusions_node',
+        name='octomap_occlusions_node',
+        output='screen',
         parameters=[
-            LaunchConfiguration("octomap_config"),
+            LaunchConfiguration('octomap_config'),
             {
-                "use_sim_time": LaunchConfiguration("use_sim_time"),
+                'use_sim_time': LaunchConfiguration('use_sim_time'),
             },
         ],
     )
